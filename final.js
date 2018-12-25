9b96f08bc41a82392cba9120b801cfc1706c70ae
Shym Barrett <shymjamin@yahoo.com>
2018-12-25 15:24:10 -0600

    initial functionality of color-change during  hover over body divs

---
+++ final.js
@@ -0,0 +1,38 @@
+
+$(document).ready(function() {
+    $(".top-right").hover(function(){
+        $(this).css("background-color", "#00ffd05b");
+        }, function(){
+        $(this).css("background-color", "transparent");
+      });
+
+    $(".top-left").hover(function(){
+        $(this).css("background-color", "#00ffd05b");
+        }, function(){
+        $(this).css("background-color", "transparent");
+      });
+
+    $(".middle-left").hover(function(){
+        $(this).css("background-color", "#00ffd05b");
+        }, function(){
+        $(this).css("background-color", "transparent");
+      });
+
+    $(".middle-right").hover(function(){
+        $(this).css("background-color", "#00ffd05b");
+        }, function(){
+        $(this).css("background-color", "transparent");
+      });
+
+    $(".bottom-left").hover(function(){
+        $(this).css("background-color", "#00ffd05b");
+        }, function(){
+        $(this).css("background-color", "transparent");
+      });
+
+    $(".bottom-right").hover(function(){
+        $(this).css("background-color", "#00ffd05b");
+        }, function(){
+        $(this).css("background-color", "transparent");
+      });
+});
