6d48019d3eeef366b10127537f6d6a133213303f
Shym Barrett <shymjamin@yahoo.com>
2019-01-08 21:25:52 -0600

    added range input and click functionality

--- final.js
+++ final.js
@@ -1,38 +1,19 @@
 
 $(document).ready(function() {
-    $(".top-right").hover(function(){
+
+    $(".top-right, .top-left, .middle-left, .middle-right, .bottom-left, .bottom-right").hover(function(){
         $(this).css("background-color", "#00ffd05b");
         }, function(){
         $(this).css("background-color", "transparent");
       });
 
-    $(".top-left").hover(function(){
-        $(this).css("background-color", "#00ffd05b");
-        }, function(){
-        $(this).css("background-color", "transparent");
-      });
+    $(".top-right, .top-left, .middle-left, .middle-right, .bottom-left, .bottom-right").click(function() {
+        var elbow = $(this).attr("title");
+        $(".highlight").html(elbow);
+    });
 
-    $(".middle-left").hover(function(){
-        $(this).css("background-color", "#00ffd05b");
-        }, function(){
-        $(this).css("background-color", "transparent");
-      });
+    function updateTextInput(val) {
+        document.getElementById('textInput').value=val; 
+        };
 
-    $(".middle-right").hover(function(){
-        $(this).css("background-color", "#00ffd05b");
-        }, function(){
-        $(this).css("background-color", "transparent");
-      });
-
-    $(".bottom-left").hover(function(){
-        $(this).css("background-color", "#00ffd05b");
-        }, function(){
-        $(this).css("background-color", "transparent");
-      });
-
-    $(".bottom-right").hover(function(){
-        $(this).css("background-color", "#00ffd05b");
-        }, function(){
-        $(this).css("background-color", "transparent");
-      });
-});
+    });
